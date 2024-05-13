a191524b3ae98cc34b8ca2acc16e4442dd54f78b
SandrZeus <sandro.bandro10@gmail.com>
2024-05-13 20:17:48 +0200

    Quiz game done

--- QuizGame.cpp
+++ QuizGame.cpp
@@ -5,26 +5,50 @@
 
 int main(){
 
-    std::string foods[5];
-    int size = sizeof(foods)/sizeof(foods[0]);
-    std::string temp;
+    std::string questions[] = {"1. What year was C++ created?: ",
+                                "2. Who invented C++?: ",
+                                "3. What is the predecessor of C++?: ",
+                                "4. Is the earth flat?: "};
     
+    std::string options[][4] = {{"A. 1969", "B. 1975", "C. 1985", "D. 1989"},
+                                {"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerber"},
+                                {"A. C", "B. C+", "C. C--", "D. B++"},
+                                {"A. yes", "B. no", "C. sometimes", "D. What's Earth>"}};
+    
+    char asnwerKey[] = {'C', 'B', 'A', 'B'};
+
+    int size = sizeof(questions)/sizeof(questions[0]);
+    char guess;
+    int score;
+
     for(int i = 0; i < size; i++){
-        std::cout << "Enter a food you like or 'q' to quit #" << i + 1 << ": ";
-        std::getline(std::cin, temp);
-        if(temp == "q"){
-            break;
+        std::cout << "*****************************" << '\n';
+        std::cout << questions[i] << '\n';
+        std::cout << "*****************************" << '\n';
+
+        for(int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++){
+            std::cout << options[i][j] << '\n';
+        }
+
+        std::cin >> guess;
+        guess = toupper(guess);
+
+        if(guess == asnwerKey[i]){
+            std::cout << "CORRECT\n";
+            score++;
         }
         else{
-            foods[i] = temp;
+            std::cout << "WRONG\n";
+            std::cout << "Answer: " << asnwerKey[i] << '\n';
         }
     }
 
-    std::cout << "You like the following food: \n";
-
-    for(int i = 0; !foods[i].empty(); i++){
-        std::cout << foods[i] << '\n';
-    }
+    std::cout << "*****************************" << '\n';
+    std::cout << "             Results            \n";
+    std::cout << "*****************************" << '\n';
+    std::cout << "Correct guesses: " << score << '\n';
+    std::cout << "Number of questions: " << size << '\n';
+    std::cout << "Score: " << (score/(double)size)*100 << '%';
 
     return 0;
 }
